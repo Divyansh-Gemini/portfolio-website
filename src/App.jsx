be4334f3e652a01e401875fe1b66c95109d891b5
Divyansh Gemini <divyanshgemini3232@gmail.com>
2024-03-02 20:51:23 +0530

    created Header

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import HomePage from "./pages/HomePage";
+import Header from "../src/components/Header/Header";
+import Footer from "./components/Footer/Footer";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <HomePage />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
