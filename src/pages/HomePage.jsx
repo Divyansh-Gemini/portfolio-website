7de4b0a45bce4bfc8db9c9e50ccd39017388ca48
Divyansh Gemini <divyanshgemini3232@gmail.com>
2024-03-03 10:38:46 +0530

    completed footer

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const HomePage = () => {
-  return <></>;
+  return (
+    <>
+      <p className="text-justify w-3/4 mx-auto h-screen"></p>
+    </>
+  );
 };
 
 export default HomePage;
