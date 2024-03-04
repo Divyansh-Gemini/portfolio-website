e867327c1a0d17fc67ef3aec915299bbb21297be
Divyansh Gemini <divyanshgemini3232@gmail.com>
2024-03-04 13:52:18 +0530

    added some untracked files

---
+++ src/components/Button.jsx
@@ -0,0 +1,67 @@
+import React from "react";
+import { Link } from "react-scroll";
+
+const Button = ({
+  text = "Click Me",
+  type = "anchor",
+  styling = "outlined",
+  src = "",
+  isDownloadable = false,
+}) => {
+  // styling for btn
+  const btnStyling = (styling) => {
+    switch (styling) {
+      case "outlined":
+        return "border-[var(--primary)] hover:bg-[var(--primary)] hover:text-[var(--background)] hover:font-semibold ";
+      case "filled":
+        return "border-[var(--background)] bg-[var(--primary)] text-[var(--background)] font-semibold hover:font-normal hover:border-[var(--primary)] hover:bg-[var(--background)] hover:text-[var(--white)]";
+    }
+  };
+
+  // actual btn component
+  const btn = () => {
+    return (
+      <button
+        className={`border rounded-xl inline-block px-4 py-2 transition duration-200 cursor-[var(--cursor-pointer)] ${btnStyling(
+          styling
+        )}`}
+      >
+        {text}
+      </button>
+    );
+  };
+
+  // return btn based on type (anchor / scroll)
+  switch (type) {
+    // open in new tab
+    case "anchor":
+      return (
+        <a
+          href={src}
+          target="_blank"
+          rel="noopener noreferrer"
+          {...(isDownloadable && { download: "Divyansh_Gemini_Resume.pdf" })}
+        >
+          {btn()}
+        </a>
+      );
+
+    // scroll to section
+    case "scroll":
+      return (
+        <Link
+          activeClass="active"
+          to={src}
+          spy={true}
+          smooth={true}
+          offset={-70}
+          duration={500}
+          style={{ cursor: "var(--cursor-pointer)" }}
+        >
+          {btn()}
+        </Link>
+      );
+  }
+};
+
+export default Button;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import HeroSection from "../components/Sections/HeroSection";
-import ExperienceSection from "../components/Sections/ExperienceSection";
-import EducationSection from "../components/Sections/EducationSection";
-import ProjectsSection from "../components/Sections/ProjectsSection";
-import AboutMeSection from "../components/Sections/AboutMeSection";
-import SkillsSection from "../components/Sections/SkillsSection";
-import ContactSection from "../components/Sections/ContactSection";
+import HeroSection from "../components/Sections/HeroSection/HeroSection";
+import ExperienceSection from "../components/Sections/ExperienceSection/ExperienceSection";
+import EducationSection from "../components/Sections/EducationSection/EducationSection";
+import ProjectsSection from "../components/Sections/ProjectsSection/ProjectsSection";
+import AboutMeSection from "../components/Sections/AboutMeSection/AboutMeSection";
+import SkillsSection from "../components/Sections/SkillsSection/SkillsSection";
+import ContactSection from "../components/Sections/ContactSection/ContactSection";
 
 const HomePage = () => {
   return (
