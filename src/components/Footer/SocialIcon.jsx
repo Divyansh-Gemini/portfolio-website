6c8f97a73944a51d679038fd7df1ef8d042762c9
Divyansh Gemini <divyanshgemini3232@gmail.com>
2024-03-07 20:21:21 +0530

    added HackerRank icon & fixed some UI issues

--- src/components/Footer/SocialIcon.jsx
+++ src/components/Footer/SocialIcon.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 
-const SocialIcon = ({ social, url = "#", size = 4 }) => {
+const SocialIcon = ({ social, url = "#" }) => {
   return (
     <a href={url} target="_blank" rel="noopener noreferrer">
       <svg
-        className={`w-${size} h-${size} md:w-5 md:h-5 hover:brightness-0 hover:invert cursor-[var(--cursor-pointer)]`}
+        className={
+          "w-4 h-4 md:w-5 md:h-5 hover:brightness-0 hover:invert cursor-[var(--cursor-pointer)]"
+        }
         viewBox="0 0 24 24"
       >
         <path d={social.path} className="fill-[var(--gray)]" />
