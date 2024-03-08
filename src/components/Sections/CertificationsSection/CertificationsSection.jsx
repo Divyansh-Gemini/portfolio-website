92c53103ca27127abd120a22a11854756ce2002e
Divyansh Gemini <divyanshgemini3232@gmail.com>
2024-03-08 12:08:04 +0530

    created certifications section

---
+++ src/components/Sections/CertificationsSection/CertificationsSection.jsx
@@ -0,0 +1,72 @@
+import React from "react";
+import Heading from "../../Heading";
+import CertificationCard from "./CertificationCard";
+import Modal from "./Modal";
+
+const CertificationsSection = () => {
+  const [selectedImageUrl, setSelectedImageUrl] = React.useState(null);
+
+  const openModal = (imageUrl) => {
+    setSelectedImageUrl(imageUrl);
+    document.body.style.overflow = "hidden";
+  };
+  const closeModal = () => {
+    setSelectedImageUrl(null);
+    document.body.style.overflow = "auto";
+  };
+
+  return (
+    <section className="xl:mb-40">
+      <Heading text="certifications" width="1/4" />
+
+      <div className="grid lg:grid-cols-2 gap-7 md:gap-10 md:max-lg:w-10/12 mx-auto">
+        {/* ML with Python by IBM */}
+        <CertificationCard
+          title="Machine Learning with Python"
+          issuer="IBM"
+          image="./src/assets/images/logos/logo-ibm.png"
+          onClick={() =>
+            openModal("./src/assets/images/certificates/certificate-ml.jpg")
+          }
+        />
+
+        {/* Fundamentals of DL by NVIDIA */}
+        <CertificationCard
+          title="Fundamentals of Deep Learning"
+          issuer="NVIDIA"
+          image="./src/assets/images/logos/logo-nvidia.png"
+          onClick={() =>
+            openModal("./src/assets/images/certificates/certificate-dl.jpg")
+          }
+        />
+
+        {/* Android by Ainwik */}
+        <CertificationCard
+          title="Android App Development"
+          issuer="Ainwik Infotech"
+          image="./src/assets/images/logos/logo-ainwik.png"
+          onClick={() =>
+            openModal("./src/assets/images/certificates/certificate-ml.jpg")
+          }
+        />
+
+        {/* Python by Google */}
+        <CertificationCard
+          title="Crash Course on Python"
+          issuer="Google"
+          image="./src/assets/images/logos/logo-google.png"
+          onClick={() =>
+            openModal("./src/assets/images/certificates/certificate-python.jpg")
+          }
+        />
+      </div>
+
+      {/* calling Modal */}
+      {selectedImageUrl && (
+        <Modal imageUrl={selectedImageUrl} onClose={closeModal} />
+      )}
+    </section>
+  );
+};
+
+export default CertificationsSection;
---
+++ src/components/Sections/CertificationsSection/Modal.jsx
@@ -0,0 +1,45 @@
+import React, { useRef } from "react";
+
+const Modal = ({ imageUrl, onClose }) => {
+  const modalRef = useRef(null);
+
+  const handleCloseOutsideClick = (event) => {
+    if (modalRef.current && !modalRef.current.contains(event.target)) onClose();
+  };
+
+  React.useEffect(
+    () => document.addEventListener("mousedown", handleCloseOutsideClick),
+    []
+  );
+
+  return (
+    // overlay
+    <div
+      className="fixed top-0 left-0 w-full h-full flex justify-center items-center bg-black 
+      bg-opacity-50 z-20 backdrop-blur-sm shadow-2xl shadow-black"
+    >
+      {/* modal card */}
+      <div
+        ref={modalRef}
+        className="flex flex-col bg-[var(--background)] rounded-xl p-4 w-11/12 sm:w-3/4 xl:w-2/5 sm:aspect-[12/10]"
+      >
+        {/* certificate image */}
+        <img
+          src={imageUrl}
+          alt="Certificate"
+          className="xxxxlg:h-96 xxxxw-fit h-full mx-auto rounded-lg"
+        />
+
+        {/* close btn */}
+        <button
+          onClick={onClose}
+          className="bg-[var(--primary)] text-[var(--background)] font-bold mx-auto py-2 px-4 rounded-md mt-4 cursor-[var(--cursor-pointer)] text-sm above-fold:text-base"
+        >
+          Close
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
